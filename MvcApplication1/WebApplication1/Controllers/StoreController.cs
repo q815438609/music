fff06a965fad8b3d5c176dec951a1f2b9c58eafe
q815438609 <815438609@qq.com>
2019-01-10 15:22:48 +0800

    aa

---
+++ MvcApplication1/WebApplication1/Controllers/StoreController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>{
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse
+        [HttpPost]
+        [ValidateAntiForgeryToken]//防止过多发布
+        public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+        //
+        // GET: /Store/Details
+        [HttpPost]
+        [ValidateAntiForgeryToken]//防止过多发布
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details/5
+        
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+    }
+}
